1c20667f104bd1c601723992c037c4f7958cb121
Laicasaane <laicasaane@outlook.com>
2022-07-15 19:46:17 +0700

    Add collection pools

---
+++ Packages/com.laicasaane.unity.pooling/Runtime/Core/System.Collections.Generic.Pooling/CollectionPools.cs
@@ -0,0 +1,29 @@
+﻿using Unity.Pooling;
+
+namespace System.Collections.Generic.Pooling
+{
+    public sealed class ListPool<T> : PoolBase<List<T>>
+    {
+        public static readonly ListPool<T> Shared = new ListPool<T>();
+    }
+
+    public sealed class QueuePool<T> : PoolBase<Queue<T>>
+    {
+        public static readonly QueuePool<T> Shared = new QueuePool<T>();
+    }
+
+    public sealed class StackPool<T> : PoolBase<Stack<T>>
+    {
+        public static readonly StackPool<T> Shared = new StackPool<T>();
+    }
+
+    public sealed class HashSetPool<T> : PoolBase<HashSet<T>>
+    {
+        public static readonly HashSetPool<T> Shared = new HashSetPool<T>();
+    }
+
+    public sealed class DictionaryPool<TKey, TValue> : PoolBase<Dictionary<TKey, TValue>>
+    {
+        public static readonly DictionaryPool<TKey, TValue> Shared = new DictionaryPool<TKey, TValue>();
+    }
+}
