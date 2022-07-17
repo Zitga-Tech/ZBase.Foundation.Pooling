60839fbe06369ce001499bdfa5f9b90dec6f8918
Laicasaane <laicasaane@outlook.com>
2022-07-17 12:50:15 +0700

    Add Unity.Pooling asmdef

---
+++ Packages/com.laicasaane.unity.pooling/Unity.Pooling/Common/DefaultDestroyHandler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Unity.Pooling
+{
+    public static class DefaultDestroyHandler<T> where T : UnityEngine.Object
+    {
+        private static Action<T> s_default = Destroy;
+
+        private static void Destroy(T instance)
+            => UnityEngine.Object.Destroy(instance);
+
+        public static void Set(Action<T> handler)
+            => s_default = handler ?? throw new ArgumentNullException(nameof(handler));
+
+        public static Action<T> Get()
+            => s_default ?? Destroy;
+    }
+}
