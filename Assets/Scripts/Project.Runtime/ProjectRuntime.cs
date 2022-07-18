7823c4ef53db86326361884c1ff00997fa6a1672
Laicasaane <laicasaane@outlook.com>
2022-07-18 18:19:05 +0700

    Update test

--- Assets/Scripts/Project.Runtime/ProjectRuntime.cs
+++ Assets/Scripts/Project.Runtime/ProjectRuntime.cs
@@ -1,9 +1,15 @@
 using UnityEngine;
 using Unity.Pooling;
+using Unity.Pooling.Components;
 
 namespace Project.Runtime
 {
     public class ProjectRuntime : MonoBehaviour
     {
+        [SerializeField]
+        private UnityPrefab<GameObject> _prefabT;
+
+        [SerializeField]
+        private UnityPrefab<string, GameObject> _prefabTKey;
     }
 }
