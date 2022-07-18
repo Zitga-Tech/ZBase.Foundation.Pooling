55599e662be54df4a77dbbbba221687f51146665
Laicasaane <laicasaane@outlook.com>
2022-07-18 18:39:47 +0700

    Add base Unity.Pooling.Components.Addressables

---
+++ Packages/com.laicasaane.unity.pooling/Unity.Pooling.Components.Addressables/Components/AssetReferencePoolComponent{T}.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Unity.Pooling.Components.Addressables
+{
+    public class AssetReferencePoolComponent<T>
+    {
+
+    }
+}
