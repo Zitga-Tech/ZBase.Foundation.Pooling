67a4fd234f1e1ed2dce726e65b01f72312e122e1
Laicasaane <laicasaane@outlook.com>
2022-07-24 22:39:49 +0700

    Add Unity.Pooling.Addressables

---
+++ Packages/com.laicasaane.unity.pooling/Unity.Pooling.Addressables/Pools/AssetRefGameObjectPool.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Unity.Pooling.AddressableAssets
+{
+    public class AssetRefGameObjectPool
+    {
+    }
+}
