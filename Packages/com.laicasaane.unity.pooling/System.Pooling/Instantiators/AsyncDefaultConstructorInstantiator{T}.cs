454bd3c6e580aeb41565c07470d0ed083583903d
Laicasaane <laicasaane@outlook.com>
2022-08-01 16:39:28 +0700

    Add instantiators that use default constructor

---
+++ Packages/com.laicasaane.unity.pooling/System.Pooling/Instantiators/AsyncDefaultConstructorInstantiator{T}.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.CompilerServices;
+using Cysharp.Threading.Tasks;
+
+namespace System.Pooling
+{
+    /// <summary>
+    /// Create a new instance of <typeparamref name="T"/> via default constructor.
+    /// </summary>
+    public struct AsyncDefaultConstructorInstantiator<T> : IAsyncInstantiable<T>
+        where T : class, new()
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public async UniTask<T> Instantiate()
+            => await UniTask.FromResult(new T());
+    }
+}
