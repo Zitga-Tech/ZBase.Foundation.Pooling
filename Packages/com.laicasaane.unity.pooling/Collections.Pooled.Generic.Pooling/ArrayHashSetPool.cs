73d440510f940e9f49e6fdefcd15074623371a0a
Laicasaane <laicasaane@outlook.com>
2022-08-30 23:28:43 +0700

    Add ArrayHashSetPool

---
+++ Packages/com.laicasaane.unity.pooling/Collections.Pooled.Generic.Pooling/ArrayHashSetPool.cs
@@ -0,0 +1,22 @@
+﻿using System.Pooling;
+using System.Runtime.CompilerServices;
+
+namespace Collections.Pooled.Generic.Pooling
+{
+    public class ArrayHashSetPool<T>
+        : Pool<ArrayHashSet<T>
+        , DefaultConstructorInstantiator<ArrayHashSet<T>>>
+    {
+        public ArrayHashSetPool()
+            : base()
+        { }
+
+        public ArrayHashSetPool(UniqueQueue<ArrayHashSet<T>> queue)
+            : base(queue)
+        { }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        protected override void ReturnPreprocess(ArrayHashSet<T> instance)
+            => instance.Clear();
+    }
+}
