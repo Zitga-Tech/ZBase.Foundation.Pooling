23880ba9996f06d2d8a5e19801ffcd8cf2db0ede
Laicasaane <laicasaane@outlook.com>
2022-08-30 23:28:33 +0700

    Add ArrayDictionaryPool

---
+++ Packages/com.laicasaane.unity.pooling/Collections.Pooled.Generic.Pooling/ArrayDictionaryPool.cs
@@ -0,0 +1,22 @@
+﻿using System.Pooling;
+using System.Runtime.CompilerServices;
+
+namespace Collections.Pooled.Generic.Pooling
+{
+    public class ArrayDictionaryPool<TKey, TValue>
+        : Pool<ArrayDictionary<TKey, TValue>
+        , DefaultConstructorInstantiator<ArrayDictionary<TKey, TValue>>>
+    {
+        public ArrayDictionaryPool()
+            : base()
+        { }
+
+        public ArrayDictionaryPool(UniqueQueue<ArrayDictionary<TKey, TValue>> queue)
+            : base(queue)
+        { }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        protected override void ReturnPreprocess(ArrayDictionary<TKey, TValue> instance)
+            => instance.Clear();
+    }
+}
